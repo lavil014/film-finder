6a0f2b0b13c624f54e425d60b461c1ba5be66aba
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-10-22 21:11:20 -0600

    New async functions added to my Js file to fetch data from TMDB API

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const tmdbKey = '3664c400672e4ce89e46442ce663847a';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
-const getGenres = ()=>{
+const getGenres = async ()=>{
   const genreRequestEndpoint = '/genre/movie/list'; 
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
 
-  console.log(`${tmdbBaseUrl}${genreRequestEndpoint}`)
+  console.log(urlFetch);
+
+  try{
+    let response = await fetch(urlFetch);
+
+    if(response.ok){
+      let jsonResponse = await response.json();
+      
+      console.log(jsonResponse);
+
+      return jsonResponse;
+    
+    }
+  }
+  catch(error){
+    console.error(error);
+  }
+
 
 };
 
