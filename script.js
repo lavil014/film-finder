06d82f87bd94903da3059b9d22b62572c820cfea
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-10-22 21:47:30 -0600

    New functions added to my JS files. Added a helper Js file to mapiluate the DOM

--- script.js
+++ script.js
@@ -14,18 +14,46 @@ const getGenres = async ()=>{
 
     if(response.ok){
       let jsonResponse = await response.json();
+      let genres = jsonResponse.genres;
       
-      console.log(jsonResponse);
 
-      return jsonResponse;
+      return genres;
     
+    }else{
+      console.error('Error with the response', response.error)
     }
   }
   catch(error){
-    console.error(error);
+    console.error('Error fething data from the API:', error);
   }
-
-
 };
 
+const getMovies = async()=>{
+
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+
+  console.log(urlToFetch)
+
+  try{
+    let response = await fetch(urlToFetch);
+    
+    if(response.ok){
+      let newResponse = await response.json();
+
+      console.log(newResponse.results);
+
+      return newResponse; 
+    }
+    
+  }
+  catch(error){
+    console.error(error)
+  }
+
+}
+
 getGenres();
+getMovies();
